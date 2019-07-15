f30b5c50fe23cf1cebff76201ed32a337b524227
john erps <yagols@johnerps.com>
2019-07-15 04:47:00 +0200

    Replaced license text with ref to LICENSE

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,13 @@
+/*
+
+Yagols v4.0
+
+(c) 2018-2019 John Erps
+
+This software is licensed under the MIT license (see LICENSE)
+
+*/
+
 const path = require('path');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -36,27 +46,24 @@ module.exports = env => {
     }
   };
 
-  if (env.prod) {
-    merge(conf, {
-      module: {
-        rules : [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
+  if (env.prod) {conf=merge(conf, {
+    module: {
+      devtool: 'source-map',
+      rules : [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
             }
           }
-        ]
-      },
-    });
-  } else {
-    merge(conf, {
-      devtool: 'cheap-module-eval-source-map'
-    });
-  }
+        }
+      ]
+    },
+  });} else {conf=merge(conf, {
+    devtool: 'cheap-module-eval-source-map'
+  });}
   return conf;
 };
